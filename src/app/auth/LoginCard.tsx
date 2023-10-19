ab99b85ddaeb112bcaa9f2365f228f2ccea02c0d
sunil u <usunil0@gmail.com>
2023-10-19 13:43:09 +0530

    adds loginCard

---
+++ src/app/auth/LoginCard.tsx
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Divider,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  NumberInput,
+  Stack,
+  Text,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+
+export const LoginCard = () => (
+  <Container
+    maxW="lg"
+    py={{ base: "12", md: "24" }}
+    px={{ base: "0", sm: "8" }}
+  >
+    <Stack spacing="8">
+      <Stack spacing="6">
+        <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+          <Heading size={{ base: "xs", md: "sm" }}>
+            Log in to your account
+          </Heading>
+        </Stack>
+      </Stack>
+      <Box
+        py={{ base: "0", sm: "8" }}
+        px={{ base: "4", sm: "10" }}
+        borderWidth={"1px"}
+        borderRadius={"10px"}
+        borderColor={"black"}
+        bg={"white"}
+      >
+        <Stack spacing="6">
+          <Stack spacing="5">
+            <InputGroup>
+              <InputLeftAddon borderStartRadius={6}>+91</InputLeftAddon>
+              <Input
+                type="tel"
+                placeholder="phone number"
+                borderRadius={0}
+                borderEndRadius={6}
+              />
+            </InputGroup>
+          </Stack>
+          <Stack spacing="6">
+            <Button>Sign in</Button>
+            <HStack>
+              <Divider />
+            </HStack>
+          </Stack>
+        </Stack>
+      </Box>
+    </Stack>
+  </Container>
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { LoginCard } from "./auth/LoginCard";
 
 export default function Home() {
   return (
-   <main className={styles.main}>
-    hey
+    <main className={styles.main}>
+      <LoginCard />
     </main>
-  )
+  );
 }
