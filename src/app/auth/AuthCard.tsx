49b7e1bf868cbb5ac20bc03c069c11f0466a83b9
sunil u <usunil0@gmail.com>
2023-11-16 15:02:50 +0530

    cleanup

--- src/app/auth/AuthCard.tsx
+++ src/app/auth/AuthCard.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import { LoginCard } from "./LoginCard";
 import OtpCard from "./OtpCard";
-import { Box, chakra } from '@chakra-ui/react'
-import { motion, isValidMotionProp } from 'framer-motion'
+import { Box, chakra } from "@chakra-ui/react";
+import { motion, isValidMotionProp } from "framer-motion";
 
 const ChakraBox = chakra(motion.div, {
   /**
    * Allow motion props and non-Chakra props to be forwarded.
    */
-  shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === "children",
 });
 
 const AuthCard = () => {
@@ -21,27 +21,30 @@ const AuthCard = () => {
   };
 
   return (
- <>  {phoneNumber ? (
-  <Box as="h2" textAlign="center" mb="6" maxW="md">
-  <ChakraBox
-  maxW="md"
-  borderWidth="1px"
-  borderRadius="lg"
-  overflow="hidden"
-  p="6"
-  m="auto"
-  mt="10"
-  initial={{ opacity: 0, y: -20 }}
-  animate={{ opacity: 1, y: 0 }}
->
-  <OtpCard phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber} />
-  </ChakraBox>
-  </Box>
-) : (
-  <LoginCard onSubmit={handleLogin} />
-)}
-</>
-    
+    <>
+      {phoneNumber ? (
+        <Box as="h2" textAlign="center" mb="6" maxW="md">
+          <ChakraBox
+            maxW="md"
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            p="6"
+            m="auto"
+            mt="10"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+          >
+            <OtpCard
+              phoneNumber={phoneNumber}
+              setPhoneNumber={setPhoneNumber}
+            />
+          </ChakraBox>
+        </Box>
+      ) : (
+        <LoginCard onSubmit={handleLogin} />
+      )}
+    </>
   );
 };
 
